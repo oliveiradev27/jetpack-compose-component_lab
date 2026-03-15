73f94373d8e368f6af8eae194cdf6d10883a1fa3
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2026-03-15 08:41:43 -0300

    add all components im sample home app

--- app/src/main/java/br/com/oliveiradev/jetpackcomposecomponentlab/MainActivity.kt
+++ app/src/main/java/br/com/oliveiradev/jetpackcomposecomponentlab/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.oliveiradev.jetpackcomposecomponentlab.concepts.statehoisting.AuthRepositoryImpl
 import br.com.oliveiradev.jetpackcomposecomponentlab.concepts.statehoisting.LoginScreen
 import br.com.oliveiradev.jetpackcomposecomponentlab.concepts.statehoisting.LoginViewModel
+import br.com.oliveiradev.jetpackcomposecomponentlab.ui.components.ExpandableCard
 import br.com.oliveiradev.jetpackcomposecomponentlab.ui.components.TypingText
 import br.com.oliveiradev.jetpackcomposecomponentlab.ui.theme.JetpackComposeComponentLabTheme
 
@@ -35,12 +43,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    TypingText(
-        text = "Hello $name! Sou o Dollynho, seu amiguinho! vamos brincar?!",
-        intervalMs = 500L,
-        showCursor = false,
-        modifier = modifier
-    )
+    Column(modifier.verticalScroll(rememberScrollState())) {
+        Spacer(modifier.height(32.dp))
+        Text(text = "Componente de Texto Dinâmico")
+        Spacer(modifier.height(16.dp))
+
+        TypingText(
+            text = "Hello $name! Sou o Dollynho, seu amiguinho! vamos brincar?!",
+            intervalMs = 500L,
+            showCursor = false,
+            modifier = modifier
+        )
+
+        Spacer(modifier.height(32.dp))
+        Text(text = "Form de Login para consolidar conceito de State Hoisting")
+        Spacer(modifier.height(16.dp))
+
+        LoginScreen(
+            viewModel = LoginViewModel(authRepository = AuthRepositoryImpl()),
+            onLoginSuccess = {}
+        )
+
+        Spacer(modifier.height(32.dp))
+        Text(text = "Card expansível")
+        Spacer(modifier.height(16.dp))
+        ExpandableCard(
+            title = "Título do card",
+            initiallyExpanded = false
+        ) {
+            Text(text = "Conteúdo do card expansível.")
+        }
+     }
 }
 
 @Preview(showBackground = true)
