bd3f5f7225ad974e9eb28c873fca1fd5944d17d8
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2026-03-15 08:49:29 -0300

    Add CenterAlignedTopAppBar to Greeting composable for improved UI structure

--- app/src/main/java/br/com/oliveiradev/jetpackcomposecomponentlab/MainActivity.kt
+++ app/src/main/java/br/com/oliveiradev/jetpackcomposecomponentlab/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,7 +45,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "JetPack Compose Lab") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors()
+            )
+        }
+    ) { innerPadding ->
         Column(
             Modifier
                 .padding(innerPadding)
