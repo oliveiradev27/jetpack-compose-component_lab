715f05ffdf7acd32750e9bbce64b5886d75bf176
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2026-03-15 08:50:58 -0300

    fix annotation missed

--- app/src/main/java/br/com/oliveiradev/jetpackcomposecomponentlab/MainActivity.kt
+++ app/src/main/java/br/com/oliveiradev/jetpackcomposecomponentlab/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -43,6 +44,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Scaffold(
